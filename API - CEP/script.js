51761ee875064850c64047d3b25a8917a40bcdde
Pablo Chagas <pabloaugustotei.chagas@gmail.com>
2025-08-24 20:51:12 -0300

    Update script.js

--- API - CEP/script.js
+++ API - CEP/script.js
@@ -18,11 +18,12 @@ const cepValido = (cep) => {
 
 const pesquisarCep = async () => {
     const cep = document.getElementById("cep").value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
     if (cepValido(cep)) {
         const dados = await fetch(url);
         const endereco = await dados.json();
             preencherFormulario(endereco);
         }
 }
+
 document.getElementById("cep").addEventListener("focusout", pesquisarCep);
